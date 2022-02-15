db4c5eba5123e19b4200996f161c8d307a59a416
optickx <83361124+optickx@users.noreply.github.com>
2022-02-15 14:22:58 +0100

    Add files via upload

--- Class/ArtistsDatabase.java
+++ Class/ArtistsDatabase.java
@@ -26,6 +26,70 @@ public abstract class ArtistsDatabase {
      * @see Concert
      */
 
+    public static void addArtist(Artist pArtist) {
+        try {
+            if (f.exists()) {
+                if (!isContained(pArtist.getID())) {
+                    MyObjectOutputStream moos = new MyObjectOutputStream();
+                    moos.writeObject(pArtist);
+                    moos.close();
+                } else {
+                    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f, true));
+                    oos.writeObject(pArtist);
+                    oos.close();
+                }
+            }
+            else {
+                // TODO: Create a new file to add the values.
+            }
+        } catch (Exception e) {
+            System.out.println("There was some sort of error.");
+        }
+    }
+
+
+    /**
+     * Private and auxiliar method to add a concert to the database.
+     * @param pCode
+     * @return boolean indicating if the concert is already.
+     */
+    private static boolean isContained(int pCode) {
+        boolean a = false;
+        if (f.exists()) {
+            int length = Util.fileLength(f);
+            try {
+                ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+                for (int i = 0; i < length && !a; i++)
+                    if (((Artist) ois.readObject()).getID() == pCode)
+                        a = true;
+                ois.close();
+            } catch (Exception e) {
+                System.out.println("Error 404 not found.");
+                a = false;
+            }
+        }
+        return a;
+    }
+
+
+/**The only way of obtaining concerts from the Database if
+     * by using the ID of an artist.
+     * @returns null in case of not finding anything.
+     */
+    public static Artist getArtistWithID(int pID) { 
+        ObjectInputStream ois = null;
+        Artist a = null;
+
+        if (!f.exists()) {
+            try {
+                ois = new ObjectInputStream(new FileInputStream(f));
+                int length = Util.fileLength(f);
+                //Concert aux;
+                for (int i = 0; i < length; i++) {
+                    a = (Artist) ois.readObject();
+                    if (a.getID() == pID)
+                        break;
+                }
     private static void alta() {
         print("¿Quieres meter una banda o un artista en solitario? [b/s]");
         char opt = Util.readChar('b', 's');
@@ -52,6 +116,7 @@ public abstract class ArtistsDatabase {
             String pRealName = Util.readText();
         return new Solo(pk(), pName, pLabel, pActive, pDebut, pRealName);
     }
+
 
     private static Band createBand() {
         print("¿Cuál es el nombre del grupo?");
